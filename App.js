1de48ba6b8bd5da321ca9519e2b87b13cd11a8f8
robinfintz <peacerobin15@gmail.com>
2021-10-12 19:35:17 -0400

    Added button to confirm selection

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
-import { Text, View, Picker, StyleSheet } from "react-native";
+import { Text, View, Picker, StyleSheet, Button, Alert} from "react-native";
 
 export default function App() {
   const [selectedValue, setSelectedValue] = useState("java");
   return (
     <View style={styles.container}>
       <Text>Pick a programming language!</Text>
-      <Picker
-        selectedValue={selectedValue}
-        style={{ fontSize: 15, height: 30, width: 100 }}
-        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-      >
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
-        <Picker.Item label="Python" value="py" />
-        <Picker.Item label="C++" value="cpp" />
-        <Picker.Item label="C" value="c" />
-        <Picker.Item label="R" value="r" />
-      </Picker>
+      <View style={styles.pick}>
+        <Picker
+          selectedValue={selectedValue}
+          style={{ fontSize: 15, height: 10, width: 150 }}
+          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+        >
+          <Picker.Item label="Java" value="System.out.println(Hello);" />
+          <Picker.Item label="JavaScript" value="console.log(Hello);" />
+          <Picker.Item label="Python" value="print(Hello)" />
+          <Picker.Item label="C++" value="cout >> Hello" />
+          <Picker.Item label="C" value="printf(Hello)" />
+        </Picker>
+      </View>
+      <Button
+        onPress={() => Alert.alert(selectedValue)}
+         title="Submit"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <Text>Thank you!</Text>
     </View>
   );
@@ -29,5 +36,9 @@ const styles = StyleSheet.create({
     paddingTop: 40,
     fontSize: 30,
     alignItems: "center"
+  },
+  pick: {
+    flex: 0.25,
+    fontSize: 10
   }
 });
